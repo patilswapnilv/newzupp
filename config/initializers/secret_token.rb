06f3f463e75c209cefee375ff0f12d8883798741
Jigar Patel <jagira@Pirate.(none)>
2011-02-20 18:54:36 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewzuppCrawler::Application.config.secret_token = '90d173ab9974d62f4bea09c9ea01a521fd9433b4be41407fa34ae1c38135295f8dad06afefa9e1d592fd704252bda3609356eaf82bdaba7efcd2bc1567ed5aa2'
