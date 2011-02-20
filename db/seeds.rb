3cedf5380ed18e4d8e43e086ccf4384e77825b43
Jigar Patel <jagira@gmail.com>
2011-02-20 20:35:17 +0530

    Add seed data. Digg, Reddit, Tweetmeme and HackerNews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# Create Sites
+Site.create(:name                   => "Digg",
+            :url                    => "digg.com",
+            :shortname              => "digg",
+            :top_stories_endpoint   => "http://services.digg.com/2.0/story.getTopNews.json",
+            :search_endpoint        => "",
+            :story_details_endpoint => "")
+
+Site.create(:name                   => "Reddit",
+            :url                    => "reddit.com",
+            :shortname              => "reddit",
+            :top_stories_endpoint   => "http://www.reddit.com/.json",
+            :search_endpoint        => "",
+            :story_details_endpoint => "http://www.reddit.com/api/info.json?url=")
+
+Site.create(:name                   => "Tweetmeme",
+            :url                    => "tweetmeme.com",
+            :shortname              => "tweetmeme",
+            :top_stories_endpoint   => "http://api.tweetmeme.com/stories/recent.json?count=25",
+            :search_endpoint        => "",
+            :story_details_endpoint => "http://api.tweetmeme.com/url_info.xml?url=")
+
+Site.create(:name                   => "HackerNews",
+            :url                    => "news.ycombinator.com",
+            :shortname              => "hn",
+            :top_stories_endpoint   => "http://api.ihackernews.com/page",
+            :search_endpoint        => "http://api.ihackernews.com/getid?url=",
+            :story_details_endpoint => "http://api.ihackernews.com/post/")
