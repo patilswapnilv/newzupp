9d1b0bcc8829b0aa66e29c96e9f3afadddaacf7d
Jigar Patel <jagira@gmail.com>
2011-02-28 18:02:43 +0530

    Remove page caching

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,5 @@
 class StoriesController < ApplicationController
 
-  caches_page :home, :digg, :reddit, :tweetmeme, :hackernews
-
   def home
     @page = FrontPage.first
     @stories = @page.stories.order("total_count DESC")
